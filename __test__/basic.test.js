4b526a35bd7b888f83d26c601332d135977e2e71
TangoSplicer <jasonmaoakley@gmail.com>
2025-09-03 15:44:30 +0100

    Delete __test__ directory

--- __test__/basic.test.js
+++
@@ -1,9 +0,0 @@
-```js
-const { getModulePaths } = require('../src/utils/paths');
-const fs = require('fs');
-
-test('getModulePaths returns correct structure', () => {
-  const paths = getModulePaths('/myapp', 'TestModule');
-  expect(paths.js).toContain('modules/TestModule/src');
-  expect(paths.ios).toContain('modules/TestModule/ios');
-});
